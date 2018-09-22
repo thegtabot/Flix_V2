835fe7055fe3f279ddb579caa6604b271bfea1be
user141707 <user141707@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-22 13:13:10 -0400

    first commit

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Avishek Thapa Magar on 9/12/18.
+//  Copyright © 2018 Avishek Thapa Magar. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
